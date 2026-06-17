3681ab556d0b4c77d6d0d5682c57a754e07843d3
Apple <kashelot@gmail.com>
2026-06-17 11:49:37 +0300

    Added move like up/down key bindings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 -- Show absolute line numbers in the gutter.
 vim.opt.number = true
 
@@ -12,3 +14,9 @@ vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+vim.keymap.set("n", "<leader>j", ":m .+1<CR>==")
+vim.keymap.set("n", "<leader>k", ":m .-2<CR>==")
+
+vim.keymap.set("v", "<leader>j", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "<leader>k", ":m '<-2<CR>gv=gv", { silent = true })
